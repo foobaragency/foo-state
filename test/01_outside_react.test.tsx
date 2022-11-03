d060197f119fd5bd2747f2e957f28760b80ee331
Lenilson Nascimento <lenilson.100@gmail.com>
2022-11-03 18:08:22 +0100

    fix(tests): improving tests for counter

--- test/01_outside_react.test.tsx
+++ test/01_outside_react.test.tsx
@@ -6,53 +6,42 @@ import { Counter } from "../examples/01_outside_react/src/Counter"
 
 import "@testing-library/jest-dom"
 
+jest.useFakeTimers()
+
 describe("Outside react:", () => {
-  test("should have initial value after timeout", async () => {
-    const { container } = render(<Counter />)
+  test("should have initial value of 0 ", async () => {
+    render(<Counter />)
 
-    const count = container.querySelector("span")
-
-    await waitFor(
-      () => {
-        expect(count).toHaveTextContent("10000")
-      },
-      { timeout: 2000 }
-    )
+    expect(screen.getByText("0")).toBeInTheDocument()
   })
 
-  test("add", async () => {
-    const { container } = render(<Counter />)
+  test("should have value of 10000 after timeout", async () => {
+    render(<Counter />)
 
-    const count = container.querySelector("span")
+    jest.runAllTimers()
 
-    await waitFor(
-      () => {
-        expect(count).toHaveTextContent("10000")
-      },
-      { timeout: 2000 }
-    )
+    await waitFor(() => expect(screen.getByText("10000")).toBeInTheDocument())
+  })
+
+  test("clicking the '+' button increments the state by 1", async () => {
+    render(<Counter />)
+
+    jest.runAllTimers()
 
     fireEvent.click(screen.getByText("+"))
     fireEvent.click(screen.getByText("+"))
 
-    expect(count).toHaveTextContent("10002")
+    await waitFor(() => expect(screen.getByText("10002")).toBeInTheDocument())
   })
 
-  test("sub", async () => {
-    const { container } = render(<Counter />)
+  test("cliking the '-' button decrements the state by 1", async () => {
+    render(<Counter />)
 
-    const count = container.querySelector("span")
-
-    await waitFor(
-      () => {
-        expect(count).toHaveTextContent("10000")
-      },
-      { timeout: 2000 }
-    )
+    jest.runAllTimers()
 
     fireEvent.click(screen.getByText("-"))
     fireEvent.click(screen.getByText("-"))
 
-    expect(count).toHaveTextContent("9998")
+    await waitFor(() => expect(screen.getByText("9998")).toBeInTheDocument())
   })
 })
